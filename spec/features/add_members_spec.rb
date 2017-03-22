6e3de136ca9bc881f0eded392f8cda97efc49f59
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-22 17:54:50 +0100

    Refactor test add members

--- spec/features/add_members_spec.rb
+++ spec/features/add_members_spec.rb
@@ -7,9 +7,9 @@ feature 'Adding a member' do
 		click_link "New Member"
 		fill_in "Name", with: "Johnny Bravo"
 		attach_file('Image', "spec/files/images/johnny_bravo.jpg")
-		fill_in "Company Name", with: "Bravo Inc."
-		fill_in "Company Website", with: "www.bravo.co"
-		click_button "Add Member"
+		fill_in "Company name", with: "Bravo Inc."
+		fill_in "Company webpage", with: "www.bravo.co"
+		click_button "Create Member"
 		expect(page).to have_content("Johnny Bravo")
 		expect(page).to have_css("img[src*='johnny_bravo.jpg']")
 	end
