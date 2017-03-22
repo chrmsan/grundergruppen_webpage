1c2582a0143fbafb2bfb0956dac6077844f4fbba
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-22 16:52:47 +0100

    Add test for adding member

---
+++ spec/features/add_members_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Adding a member' do
+
+	scenario 'can add a member to the index page' do
+		visit '/'
+		click_link "Add Member"
+		fill_in "Name", with: "Johnny Bravo"
+		attach_file('Profile Picture', "spec/files/images/johnny_bravo.jpg")
+		fill_in "Company Name", with: "Bravo Inc."
+		fill_in "Company Website", with: "www.bravo.co"
+		click_button "Add Member"
+		expect(page).to have_content("Johnny Bravo")
+		expect(page).to have_css("img[src*='johnny_bravo.jpg']")
+	end
+end
