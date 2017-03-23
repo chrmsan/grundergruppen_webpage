11916f3d985d156fe393b16381ad47726df11439
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 09:55:19 +0100

    Add code to save members params to database

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,5 +8,15 @@ class MembersController < ApplicationController
 	end
 
 	def create
+		@member = Member.create(post_params)
+		redirect_to members_path
 	end
+
+
+	private
+
+	def post_params
+		params.require(:member).permit(:name, :image, :company_name, :company_webpage)
+	end
+
 end
