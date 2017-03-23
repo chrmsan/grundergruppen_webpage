f3802b0fdc20c7210e9e5af18981d5131a84e622
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 14:56:10 +0100

    Define default factory for Member

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :member do
-    name "MyString"
-    company_name "MyString"
-    company_webopage "MyString"
+    name "Johnny Bravo"
+    image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/johnny_bravo.jpg', 'image/jpg')
+    company_name "Bravo Inc."
+    company_webopage "www.bravo.co"
   end
 end
