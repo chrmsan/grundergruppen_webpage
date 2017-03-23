010148a61471f5bdca747dfbc6b753a1d5fbf53f
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 15:35:07 +0100

    Add test for editing a member

---
+++ spec/features/edit_member_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Editing a Member' do
+	background do 
+		member = create(:member)
+		visit '/'
+		find(:xpath, "//a[contains(@href,'members/1/edit')]").click
+		click_link "Edit Member"
+	end
+
+	scenario "Can edit a member" do
+		fill_in "Name", with: "Jonathan Bravo"
+		click_button "Update Member"
+		expect(page).to have_content("Member updated")
+		expect(page).to have_content("Jonathan Bravo")
+		expect(page).to_not have_content("Johnny Bravo")
+	end
+end
