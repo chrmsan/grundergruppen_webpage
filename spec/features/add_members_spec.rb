27dab353d409b3d88dacd18807a5a2c218059792
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 14:29:40 +0100

    Add test to add an error when making a member without an image

--- spec/features/add_members_spec.rb
+++ spec/features/add_members_spec.rb
@@ -4,7 +4,7 @@ feature 'Adding a member' do
 
 	scenario 'can add a member to the index page' do
 		visit '/'
-		click_link "New Member"
+		click_link "Add New Member"
 		fill_in "Name", with: "Johnny Bravo"
 		attach_file('Image', "spec/files/images/johnny_bravo.jpg")
 		fill_in "Company name", with: "Bravo Inc."
@@ -13,4 +13,15 @@ feature 'Adding a member' do
 		expect(page).to have_content("Johnny Bravo")
 		expect(page).to have_css("img[src*='johnny_bravo.jpg']")
 	end
+
+	scenario 'needs and to create a member' do
+		visit '/'
+		click_link 'Add New Member'
+		fill_in "Name", with: "Johnny Bravo"
+		fill_in "Company name", with: "Bravo Inc."
+		fill_in "Company webpage", with: "www.bravo.co"
+		click_button "Create Member"
+		expect(page).to have_content("You need an image to create a member.")
+	end
+
 end
