51b481ace0cfc32986c19211248d962cba773445
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 15:34:25 +0100

    Fix typo in member factory

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     name "Johnny Bravo"
     image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/johnny_bravo.jpg', 'image/jpg')
     company_name "Bravo Inc."
-    company_webopage "www.bravo.co"
+    company_webpage "www.bravo.co"
   end
 end
