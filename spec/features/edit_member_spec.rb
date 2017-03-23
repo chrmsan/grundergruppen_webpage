9a6930e570286c51134527ed11aa66808659abea
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-23 16:44:08 +0100

    Refactor test with correct href

--- spec/features/edit_member_spec.rb
+++ spec/features/edit_member_spec.rb
@@ -4,14 +4,14 @@ feature 'Editing a Member' do
 	background do 
 		member = create(:member)
 		visit '/'
-		find(:xpath, "//a[contains(@href,'members/1')]").click
-		click_link "Edit Member"
+		find("a[href='#{edit_member_path(member)}']").click
+		#click_link "Edit Member"
 	end
 
 	scenario "Can edit a member" do
 		fill_in "Name", with: "Jonathan Bravo"
 		click_button "Update Member"
-		expect(page).to have_content("Member updated")
+		expect(page).to have_content("Member Updated!")
 		expect(page).to have_content("Jonathan Bravo")
 		expect(page).to_not have_content("Johnny Bravo")
 	end
